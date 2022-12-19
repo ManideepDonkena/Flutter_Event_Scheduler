7aec3e87939be831bf1e5bd38e39beab549212e8
ManideepDonkena <manideep21@iitk.ac.in>
2022-12-19 11:32:44 +0530

    nothing

--- event_listener/lib/new_event.dart
+++ event_listener/lib/new_event.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class Event extends StatefulWidget {
@@ -10,18 +9,45 @@ class Event extends StatefulWidget {
 
 class _EventState extends State<Event> {
   final TextEditingController _textEditingController = TextEditingController();
+  void _showDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.amberAccent,
+            title: const Text(
+              'Alert!!',
+              style: TextStyle(color: Colors.red, fontSize: 12),
+            ),
+            content: const Text("""
+Please type something you haven't typed anything"""),
+            actions: [
+              MaterialButton(
+                onPressed: () {},
+                child: const Text("Button"),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("cancel"),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 151, 140, 219),
       appBar: AppBar(
-        title:  const Text(
+        title: const Text(
           'New Event',
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
-
         ),
         // centerTitle: true,
         automaticallyImplyLeading: false,
@@ -33,11 +59,17 @@ class _EventState extends State<Event> {
             )),
         backgroundColor: const Color.fromARGB(255, 75, 57, 185),
       ),
-
       body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const Center(
+              child: Image(
+                image: AssetImage('new.jpg'),
+                width: 346,
+                height: 80,
+              ),
+            ),
             Center(
               child: SizedBox(
                 width: 182 * 2,
@@ -60,7 +92,9 @@ class _EventState extends State<Event> {
                       floatingLabelBehavior: FloatingLabelBehavior.auto,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(4000.0)),
-                        borderSide: BorderSide(color: Color.fromARGB(255, 75, 57, 185),),
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 75, 57, 185),
+                        ),
                       ),
                       fillColor: Color.fromARGB(255, 75, 57, 185),
                       prefixIcon: Icon(
@@ -74,8 +108,6 @@ class _EventState extends State<Event> {
                         color: Colors.white,
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
-
-
                       ),
                     ),
                   ),
@@ -85,45 +117,37 @@ class _EventState extends State<Event> {
             const SizedBox(
               height: 10,
             ),
-
             Center(
-              // child: Container(
-              //   width: 182 * 2,
-              //   height: 48,
-              //   decoration: BoxDecoration(
-              //       borderRadius: BorderRadius.circular(24),
-              //       color: const Color.fromARGB(255, 185, 178, 231)),
-              child:  Center(
-                child: Builder(
-                    builder: (context) {
-                      return ElevatedButton(
-
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(364, 48),
-                            backgroundColor: const Color.fromARGB(255, 185, 178, 231),
-                            alignment: const Alignment(0,0),
-                            shadowColor: const Color.fromARGB(255, 185, 178, 231),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24)
-                            )
-                        ),
-                        onPressed: () async {
-                          String newEventText = _textEditingController.text;
-                          if ( newEventText.trim().isNotEmpty){
-                            Navigator.of(context).pop(newEventText);
-                          }
-                        },
-                        child: const Text(
-                          "ADD",
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 246, 246, 251),
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      );
-                    }
-                ),
-
+              child: Center(
+                child: Builder(builder: (context) {
+                  return ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(364, 48),
+                        backgroundColor:
+                            const Color.fromARGB(255, 185, 178, 231),
+                        alignment: const Alignment(0, 0),
+                        shadowColor: const Color.fromARGB(255, 185, 178, 231),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24))),
+                    onPressed: () async {
+                      String newEventText = _textEditingController.text;
+                      if (newEventText.trim().isNotEmpty) {
+                        Navigator.of(context).pop(newEventText);
+                      } else {
+                        _showDialog();
+                      }
+                    },
+                    label: const Text(
+                      "ADD",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 246, 246, 251),
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    icon: const Icon(Icons.add_circle_rounded,
+                        color: Colors.deepPurpleAccent),
+                  );
+                }),
               ),
             ),
           ]),
